c7f69a2f07d57bc1ac4e9c71a7cf5999d8a24abd
Sung-il Yoon <books13456@gmail.com>
2022-11-09 13:49:55 +0900

    link stdin and stdout

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main() {
+    clock_t start = clock();
     printf("Hello world!\n");
     long long i = 1000000000;
-    while(i--);    
+    while(i--);
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+
+    clock_t end = clock();
+    printf("Time taken: %lf\n", (double)(end - start) / CLOCKS_PER_SEC);
     return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use seccompiler::{BpfProgram, SeccompAction, SeccompFilter};
 use std::{convert::TryInto, ffi::c_char};
 
-//todo get accurate memory size of a.out
+// This program does not work on aarch64.
+// because the syscall number is different.
 
 fn main() {
     //compile main.c to a.out with gcc
@@ -9,6 +10,7 @@ fn main() {
         libc::system("gcc main.c\0".as_ptr() as *const c_char);
     }
 
+    // set rlimit
     let rlim_mem = libc::rlimit {
         rlim_cur: 250000000,
         rlim_max: libc::RLIM_INFINITY,
@@ -23,12 +25,24 @@ fn main() {
         libc::setrlimit(libc::RLIMIT_CPU, &rlim_cpu);
     }
 
+    // init rusage
     let mut ruse: libc::rusage = unsafe { std::mem::zeroed() };
+
+    // open input file and output file
+    let fd_in = unsafe { libc::open("input1\0".as_ptr() as *const c_char, libc::O_RDONLY) };
+    let fd_out = unsafe {
+        libc::open(
+            "output1\0".as_ptr() as *const c_char,
+            libc::O_WRONLY | libc::O_CREAT,
+            0644,
+        )
+    };
 
     let pid = unsafe { libc::fork() };
     if pid == 0 {
         // Child process
 
+        // set seccomp
         // Load the seccomp filter
         let filter: BpfProgram = SeccompFilter::new(
             vec![(libc::SYS_open, vec![])].into_iter().collect(),
@@ -41,28 +55,67 @@ fn main() {
         .unwrap();
         seccompiler::apply_filter(&filter).unwrap();
 
-        unsafe {
-            //run a.out
-            libc::execve(
-                "/usr/bin/time\0".as_ptr() as *const c_char,
-                [
-                    "/usr/bin/time\0".as_ptr() as *const c_char,
-                    "./a.out\0".as_ptr() as *const c_char,
+        let pid_c = unsafe { libc::fork() };
+        if pid_c == 0 {
+            unsafe {
+                // link stdin and stdout
+                libc::dup2(fd_in, libc::STDIN_FILENO);
+                libc::dup2(fd_out, libc::STDOUT_FILENO);
+
+                //run a.out
+                libc::execve(
+                    "a.out\0".as_ptr() as *const c_char,
                     std::ptr::null(),
-                ]
-                .as_ptr(),
-                std::ptr::null(),
+                    std::ptr::null(),
+                );
+
+                // get time using /usr/bin/time
+                // libc::execve(
+                //     "/usr/bin/time\0".as_ptr() as *const c_char,
+                //     [
+                //         "/usr/bin/time\0".as_ptr() as *const c_char,
+                //         "./a.out\0".as_ptr() as *const c_char,
+                //         std::ptr::null(),
+                //     ]
+                //     .as_ptr(),
+                //     std::ptr::null(),
+                // );
+            }
+        } else if pid_c > 0 {
+            // wait for child process
+            let mut status: libc::c_int = 0;
+            unsafe { libc::wait(&mut status) };
+
+            // close file descriptors
+            unsafe {
+                libc::close(fd_in);
+                libc::close(fd_out);
+            }
+
+            println!("main.c exited with status {}", status);
+
+            // get resource usage from child process
+            unsafe { libc::getrusage(libc::RUSAGE_CHILDREN, &mut ruse) };
+
+            println!("memory usage: {} kb", ruse.ru_maxrss);
+            println!(
+                "time usage: {}{} ms",
+                ruse.ru_utime.tv_sec,
+                ruse.ru_utime.tv_usec / 1000
             );
+
+            // release seccomp filter
+            unsafe {
+                libc::prctl(libc::PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
+            }
+        } else {
+            panic!("fork failed");
         }
     } else if pid > 0 {
         // Parent process
+        // wait for child process
         let mut status: libc::c_int = 0;
         unsafe { libc::wait(&mut status) };
-
-        unsafe { libc::getrusage(libc::RUSAGE_CHILDREN, &mut ruse) };
-
-        println!("memory usage: {} kb", ruse.ru_maxrss);
-        println!("time usage: {} ms", ruse.ru_utime.tv_usec / 1000);
 
         println!("Child exited with status {}", status);
     } else {
